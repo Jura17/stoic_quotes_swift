3caf3dafd72bc46f37283139b37acbd0fe2d78a9
julianrakow <julian.rakow@yahoo.com>
2023-10-11 20:17:33 +0200

    Initial Commit

---
+++ StoicQuotes/StoicQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoicQuotesApp.swift
+//  StoicQuotes
+//
+//  Created by Julian Rakow on 11.10.23.
+//
+
+import SwiftUI
+
+@main
+struct StoicQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
